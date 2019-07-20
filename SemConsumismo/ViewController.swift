b3f0a825c2668ce70ed68028da0a1af93dfae77d
lffattori <luhf.fattori@icloud.com>
2019-07-19 21:24:26 -0300

    Storyboard do tutorial feita, mas ainda tem erros + quebra de impulsos pronta

--- SemConsumismo/ViewController.swift
+++ SemConsumismo/ViewController.swift
@@ -2,19 +2,38 @@
 //  ViewController.swift
 //  SemConsumismo
 //
-//  Created by Luiza Fattori on 16/07/19.
+//  Created by Luiza Fattori on 19/07/19.
 //  Copyright © 2019 Luiza Fattori. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var controlePagina: UIPageControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    private var pageControl:UIPageControl{
+        let pc = UIPageControl()
+        pc.currentPage = 0
+        pc.numberOfPages = 3
+        
+        return pc
+    }
+    
 
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
--- SemConsumismo/ViewControllerHistoricoPage.swift
+++ SemConsumismo/ViewControllerHistoricoPage.swift
@@ -9,14 +9,18 @@
 import UIKit
 
 class ViewControllerHistoricoPage: UIViewController {
-
+    
+    @IBAction func botaoVoltar(_ sender: Any) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
 //        self.navigationItem.setHidesBackButton(true, animated:true)
 
         // Do any additional setup after loading the view.
     }
-    
 
     /*
     // MARK: - Navigation
